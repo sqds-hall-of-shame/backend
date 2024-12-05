74ff83563c9ff5a06ccd8885f14cdcc8e2fa0538
duccdev <duccybaka@gmail.com>
2024-12-05 10:33:04 +0300

    fix: avatars

--- utils.py
+++ utils.py
@@ -3,37 +3,54 @@ import json
 import config
 from typing import Any, Callable
 
+
 def get_messages() -> dict[str, dict]:
     with open(f"{config.DATABASE_STORAGE}/messages.json") as f:
-        return json.load(f)
+        messages = json.load(f)
+
+    users = get_users()
+    for id in messages:
+        messages[id]["uploader"] = users[messages[id]["uploader"]["id"]]
+
+    return messages
+
 
 def get_users() -> dict[str, dict]:
     with open(f"{config.DATABASE_STORAGE}/users.json") as f:
         return json.load(f)
 
+
 def get_attachments(id: int) -> dict[str, dict] | None:
     filename = f"{id}.json"
-    
+
     if filename in os.listdir(f"{config.DATABASE_STORAGE}/messages"):
         with open(f"{config.DATABASE_STORAGE}/messages/{filename}") as f:
             return json.load(f).get("attachments")
+
 
 def get_statistics() -> dict[str, Any]:
     with open(f"{config.DATABASE_STORAGE}/statistics.json") as f:
         return json.load(f)
 
+
 def science(metric: str) -> None:
     with open(f"{config.DATABASE_STORAGE}/science.json", encoding="utf-8") as f:
         science = json.load(f)
-    
+
     science.update({metric: science.get(metric, 0) + 1})
-    
+
     with open(f"{config.DATABASE_STORAGE}/science.json", "w", encoding="utf-8") as f:
         json.dump(science, f, indent=2)
 
-def paginate(array: list | tuple | set, count: int = 100, debug: bool = False, logger: Callable = print) -> list:
+
+def paginate(
+    array: list | tuple | set,
+    count: int = 100,
+    debug: bool = False,
+    logger: Callable = print,
+) -> list:
     """Efficient paginator for making a multiple lists out of a list which are sorted in a page-vise order.
-    
+
     For example:
     ```py
     >>> abc = [1, 2, 3, 4, 5]
@@ -45,19 +62,19 @@ def paginate(array: list | tuple | set, count: int = 100, debug: bool = False, l
     """
     array = list(array)
     paginated = []
-    
+
     temp = []
     for item in array:
         if len(temp) == count:
             paginated.append(temp)
             temp = []
-        
+
         temp.append(item)
-        
+
         if debug:
             logger(len(temp), item)
-    
+
     if len(temp) > 0:
         paginated.append(temp)
-    
+
     return paginated
